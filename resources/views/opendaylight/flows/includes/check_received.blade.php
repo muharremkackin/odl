7f5faf3015fe0264251c75b58eef5dc7758cab72
Muharrem Kaçkın <muharremkackin@gmail.com>
2020-06-14 21:22:34 +0300

    Yeni bildirim

--- resources/views/opendaylight/flows/includes/check_received.blade.php
+++ resources/views/opendaylight/flows/includes/check_received.blade.php
@@ -7,7 +7,11 @@
     @elseif($flow['main-received'] > 100)
         <div class="w-screen h-16 bg-red-300">
             <p>Sistem saldiri altinda</p>
-            <p>{{ $flow['main-flow'] }} portunda yogunluk var</p>
+            <p>{{ $flow['main-flow'] }} portunda saldiri var</p>
+        </div>
+    @else
+        <div class="w-screen h-16 bg-green-300">
+            <p class="font-bold text-sm">Sistem guvende</p>
         </div>
     @endif
 @endforeach
