dc7a038fda0e5a322122b667bdf8818f42aff6ec
Muharrem Kaçkın <muharremkackin@gmail.com>
2020-06-14 21:30:57 +0300

    Safety variable added

--- resources/views/opendaylight/flows/includes/check_received.blade.php
+++ resources/views/opendaylight/flows/includes/check_received.blade.php
@@ -1,3 +1,6 @@
+@php
+  $safety = true;
+@endphp
 @foreach($flows as $flow)
     @if($flow['main-received'] >= 75 && $flow['main-received']<= 100)
         <div class="w-screen h-16 bg-yellow-200">
@@ -5,7 +8,7 @@
             <p>{{ $flow['main-flow'] }} portunda yogunluk var</p>
         </div>
         @php
-        $flows['safety'] = false;
+        $safety = false
         @endphp
     @elseif($flow['main-received'] > 100)
         <div class="w-screen h-16 bg-red-300">
@@ -13,12 +16,12 @@
             <p>{{ $flow['main-flow'] }} portunda saldiri var</p>
         </div>
         @php
-            $flows['safety'] = false;
+            $safety = false;
         @endphp
     @endif
 @endforeach
 
-@if($flows['safety'])
+@if($safety)
     <div class="w-screen h-16 bg-green-300">
         <p>Sistem guvende</p>
     </div>
